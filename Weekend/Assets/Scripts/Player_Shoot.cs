876c120f710db04abec8787f2b4e8d7dee36f103
Yukari9463 <planetside2@naver.com>
2019-11-20 20:16:44 +0900

    reset for gun
    d

--- Weekend/Assets/Scripts/Player_Shoot.cs
+++ Weekend/Assets/Scripts/Player_Shoot.cs
@@ -3,10 +3,6 @@
 public class Player_Shoot : MonoBehaviour
 {
     private Camera cam;
-    public Camera fpsCam;
-    public float damage = 10f;
-    public float range = 500f;
-
 
     void Start()
     {
@@ -15,17 +11,10 @@ public class Player_Shoot : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) 
-        {
-            Shoot();
-        }
+        
     }
 
     void Shoot() {
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-        }
+        
     }
 }
---
+++ Weekend/Assets/Scripts/Player_State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player_State : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void TakeDamage (float amount) {
+        health -= amount;
+        if(health <= 0f) {
+            Die();
+        }
+    }
+
+    void Die() {
+        Destroy(gameObject);
+    }
+}
